69c80315c09a16a5737ccdc92cb24d93598f3ec1
mj.kim <alswo9853@gmail.com>
2023-10-11 12:45:19 +0900

    프로그래머스 - 주식가격

--- Programmers/stack-queue/주식가격/sol.py
+++ Programmers/stack-queue/주식가격/sol.py
@@ -9,7 +9,6 @@ def solution(prices):
     while prices_queue:
         tmp_a = prices_queue.popleft()
         tmp = 0
-
         for a in prices_queue:
             tmp += 1
             if tmp_a > a: break
