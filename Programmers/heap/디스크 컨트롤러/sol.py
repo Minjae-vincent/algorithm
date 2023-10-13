bb45e21d93941f923a9134447e27bcc1829f0493
mj.kim <alswo9853@gmail.com>
2023-10-14 00:57:02 +0900

    프로그래머스 - heap

--- Programmers/heap/디스크 컨트롤러/sol.py
+++ Programmers/heap/디스크 컨트롤러/sol.py
@@ -2,18 +2,18 @@ import heapq
 
 def solution(jobs):
     answer, now, index = 0, 0, 0
-    end_time = -1
+    start = -1
     heap = []
 
     while index < len(jobs):
         for a in jobs:
-            if end_time < a[0] <= now:
+            if start < a[0] <= now:
                 heapq.heappush(heap, [a[1], a[0]])
         print(heap)
 
         if len(heap) > 0:
             cur = heapq.heappop(heap)
-            end_time = now
+            start = now
             now += cur[0]
             answer += now - cur[1]
             index += 1
