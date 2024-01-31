6b2327c6454f30648d58034a13eb54760e1bb1a8
mj.kim <alswo9853@gmail.com>
2024-01-31 20:09:39 +0900

    s

--- BaekJoon/brute_force/4375/prob_num.py
+++ BaekJoon/brute_force/4375/prob_num.py
@@ -1,5 +1,18 @@
-def main():
+import sys
 
+
+def main():
+    while True:
+        try:
+            n = int(input())  # 정수 n
+        except:
+            break
+        tmp = ''
+        while True:
+            tmp += '1'
+            if int(tmp) % n == 0:
+                print(len(tmp))
+                break
 
 if __name__ == '__main__':
     main()
